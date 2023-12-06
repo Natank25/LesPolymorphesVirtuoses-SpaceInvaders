c345ee0c5cb608180ae609db006d53e3651f6fba
Natank25 <natank2511@gmail.com>
2023-12-06 14:34:12 +0100

    Pour Armand

--- python/classes.py
+++ python/classes.py
@@ -1,28 +1,320 @@
-import pygame.display
-
-import main_game
-import random
-import sys
 import os
+import random
+from enum import Enum
+
 import pygame
+from pygame.sprite import AbstractGroup
 
-class Ennemi:
 
-    NbEnnemis = random.randint(10, 20)
+class GameConstants:
+    win_size = []
 
-    def __init__(self, speed):
-        self.depart = random.randint(0, pygame.display.get_window_size()[0])
-        self.hauteur = 0
-        self.spee
+    difficulty = 1
 
+    class GameEvents(Enum):
+        # common
+        CommonInvader1SpawnEvent = pygame.USEREVENT + 1
+        CommonInvader2SpawnEvent = pygame.USEREVENT + 2
+
+        # speed
+        SpeedInvader1SpawnEvent = pygame.USEREVENT + 3
+        SpeedInvader2SpawnEvent = pygame.USEREVENT + 4
+
+        # tank
+        TankInvader1SpawnEvent = pygame.USEREVENT + 5
+
+        # boss
+        Boss1SpawnEvent = pygame.USEREVENT + 6
+
+    class PlayerSpeed(Enum):
+        LEFT = -2
+        STOP = 0
+        RIGHT = 2
+
+    class EnemyAttributes(Enum):
+        # common
+        CommonInvaders1DefaultHealth = 1
+        CommonInvaders1DefaultSpeedx = 0.3
+        CommonInvaders1DefaultSpeedy = 0.1
+
+        CommonInvaders2DefaultHealth = 5
+        CommonInvaders2DefaultSpeedx = 0.3
+        CommonInvaders2DefaultSpeedy = 0.1
+
+        # speed
+        SpeedInvader1DefaultHealth = 1
+        SpeedInvader1DefaultSpeedx = 0.4
+        SpeedInvader1DefaultSpeedy = 0.3
+
+        SpeedInvader2DefaultHealth = 3
+        SpeedInvader2DefaultSpeedx = 0.5
+        SpeedInvader2DefaultSpeedy = 0.4
+
+        # tank
+        TankInvader1DefaultHealth = 20
+        TankInvader1DefaultSpeedx = 0.2
+        TankInvader1DefaultSpeedy = 0.075
+
+        # Boss
+        Boss1DefaultHealth = 150
+        Boss1DefaultSpeedx = 2
+        Boss1DefaultSpeedy = 0.2
+        Boss1DefaultSpeedATK = 1
+
+    class EnemyType(Enum):
+        # common
+        COMMONINVADER1 = "CommonInvader1"
+        COMMONINVADER2 = "CommonInvader2"
+
+        # speed
+        SPEEDINVADER1 = "SpeedInvader1"
+        SPEEDINVADER2 = "SpeedInvader2"
+
+        # tank
+        TANKINVADER1 = "TankInvader1"
+
+        # Boss
+        BOSS1 = "Boss1"
+
+class Balle:
+    def __init__(self):
+        self.image = os.path.join('..', "img", "balle.png")
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos, img_name, file_ext="png", *groups: AbstractGroup):
+        super().__init__(*groups)
+        self.image = pygame.image.load(os.path.join('..', "img", img_name +
+                                                    "." + file_ext))
+        self.pos = pos
+        self.speed = GameConstants.PlayerSpeed.STOP
+
+    def bouger(self):
+        self.pos[0] += self.speed.value
+
+    def setSpeed(self, speed: GameConstants.PlayerSpeed):
+        self.speed = speed
+
+    def tirer(self):
+        pygame.image.load(os.path.join('..', "img", "balle.png"))
+
+
+
+class Boss(pygame.sprite.Sprite):
+    def __init__(self, hauteur, speedx, speedy, health, ATKspeed, img_name, file_ext="png", *groups: AbstractGroup):
+        super().__init__(*groups)
+        self.hauteur = hauteur
+        self.speedx = speedx
+        self.speedy = speedy
+        self.health = health
+        self.ATKspeed = ATKspeed
+        self.img_name = pygame.image.load(os.path.join('..', "img", img_name +
+                                                       "." + file_ext))
+
+
+class Boss1(Boss):
+    def __init__(self, hauteur=25):
+        super().__init__(hauteur,
+                         GameConstants.EnemyAttributes.Boss1DefaultSpeedx.value + (
+                                 GameConstants.difficulty / 10),
+                         GameConstants.EnemyAttributes.Boss1DefaultSpeedy.value + (
+                                 GameConstants.difficulty / 20),
+                         GameConstants.EnemyAttributes.Boss1DefaultHealth.value * (
+                                 1 + GameConstants.difficulty),
+                         GameConstants.EnemyAttributes.Boss1DefaultSpeedATK.value + GameConstants.difficulty,
+                         img_name="CommonInvader1")
+
+
+class Invader(pygame.sprite.Sprite):
+    def __init__(self, hauteur, speedx, speedy, health, img_name, file_ext="png", *groups: AbstractGroup):
+        super().__init__(*groups)
+        self.speedx = speedx
+        self.speedy = speedy
+        self.depart = random.randint(0, GameConstants.win_size[0])
+        self.hauteur = hauteur
+        self.image = pygame.image.load(os.path.join('..', "img", img_name +
+                                                    "." + file_ext))
+        self.health = health
 
     def avancer(self):
-        self.hauteur -= self.speed
+        self.hauteur += self.speedx
+        if (self.depart < 0 or self.depart >
+                pygame.display.get_window_size()[0]):
+            self.speedy = -self.speedy
 
-class Invader1(Ennemi):
-    def __init__(self):
-        super(Invader1, self).__init__(5)
+        self.depart += self.speedy
 
-class Invader2(Ennemi):
+
+class CommonInvader1(Invader):
     def __init__(self):
-        pass
+        super().__init__(0,
+                         GameConstants.EnemyAttributes.CommonInvaders1DefaultSpeedx.value + (
+                                 GameConstants.difficulty / 10),
+                         GameConstants.EnemyAttributes.CommonInvaders1DefaultSpeedy.value + (
+                                 GameConstants.difficulty / 20),
+                         GameConstants.EnemyAttributes.CommonInvaders1DefaultHealth.value * (
+                                 1 + GameConstants.difficulty),
+                         "CommonInvader1")
+
+
+class CommonInvader2(Invader):
+    def __init__(self):
+        super().__init__(0,
+                         GameConstants.EnemyAttributes.CommonInvaders2DefaultSpeedx.value + (
+                                 GameConstants.difficulty / 10),
+                         GameConstants.EnemyAttributes.CommonInvaders2DefaultSpeedy.value + (
+                                 GameConstants.difficulty / 20),
+                         GameConstants.EnemyAttributes.CommonInvaders2DefaultHealth.value * (
+                                 1 + GameConstants.difficulty),
+                         "CommonInvader2")
+
+
+class SpeedInvader1(Invader):
+    def __init__(self):
+        super().__init__(0,
+                         GameConstants.EnemyAttributes.SpeedInvader1DefaultSpeedx.value + (
+                                 GameConstants.difficulty / 10),
+                         GameConstants.EnemyAttributes.SpeedInvader1DefaultSpeedy.value + (
+                                 GameConstants.difficulty / 20),
+                         GameConstants.EnemyAttributes.SpeedInvader2DefaultHealth.value * (
+                                 1 + GameConstants.difficulty),
+                         "SpeedInvader1")
+
+
+class SpeedInvader2(Invader):
+    def __init__(self):
+        super().__init__(0,
+                         GameConstants.EnemyAttributes.SpeedInvader2DefaultSpeedx.value + (
+                                 GameConstants.difficulty / 10),
+                         GameConstants.EnemyAttributes.SpeedInvader2DefaultSpeedy.value + (
+                                 GameConstants.difficulty / 20),
+                         GameConstants.EnemyAttributes.SpeedInvader2DefaultHealth.value * (
+                                 1 + GameConstants.difficulty),
+                         "SpeedInvader2")
+
+
+class TankInvader1(Invader):
+    def __init__(self):
+        super().__init__(0,
+                         GameConstants.EnemyAttributes.TankInvader1DefaultSpeedx.value + (
+                                 GameConstants.difficulty / 10),
+                         GameConstants.EnemyAttributes.TankInvader1DefaultSpeedy.value + (
+                                 GameConstants.difficulty / 20),
+                         GameConstants.EnemyAttributes.TankInvader1DefaultHealth.value * (
+                                 1 + GameConstants.difficulty),
+                         "SpeedInvader1")
+
+
+class EnemiesManager:
+    list_enemies = []
+
+    @staticmethod
+    def send_waves_endless(difficulty: int):
+        for i in range(difficulty * 3):
+            pass
+
+    @staticmethod
+    def send_waves_levels(num_lvl: int):
+        match num_lvl:
+            case 1:
+                pygame.time.set_timer(
+                    GameConstants.GameEvents.CommonInvader1SpawnEvent.value,
+                    1500, 3)
+
+            case 2:
+                for i in range(2):
+                    pygame.time.set_timer(
+                        GameConstants.GameEvents.CommonInvader1SpawnEvent.value,
+                        1500)
+                    pygame.time.set_timer(
+                        GameConstants.GameEvents.SpeedInvader1SpawnEvent.value,
+                        3000)
+            case 3:
+                pygame.time.set_timer(
+                    GameConstants.GameEvents.CommonInvader1SpawnEvent.value,
+                    1500, 4)
+                pygame.time.set_timer(
+                    GameConstants.GameEvents.SpeedInvader1SpawnEvent.value,
+                    3000, 2)
+            case 4:
+                for i in range(3):
+                    pygame.time.set_timer(
+                        GameConstants.GameEvents.CommonInvader1SpawnEvent.value,
+                        1500, 2)
+                    pygame.time.set_timer(
+                        GameConstants.GameEvents.SpeedInvader1SpawnEvent.value,
+                        3000)
+            case 5:
+                pygame.time.set_timer(
+                    GameConstants.GameEvents.Boss1SpawnEvent.value, 0)
+            case 6:
+                for i in range(3):
+                    pygame.time.set_timer(
+                        GameConstants.GameEvents.CommonInvader1SpawnEvent.value,
+                        1500, 2)
+                    pygame.time.set_timer(
+                        GameConstants.GameEvents.SpeedInvader1SpawnEvent.value,
+                        3000)
+                    pygame.time.set_timer(
+                        GameConstants.GameEvents.CommonInvader2SpawnEvent.value,
+                        4500)
+            case 7:
+                for i in range(3):
+                    pygame.time.set_timer(
+                        GameConstants.GameEvents.CommonInvader2SpawnEvent.value,
+                        1500, 2)
+                    pygame.time.set_timer(
+                        GameConstants.GameEvents.SpeedInvader1SpawnEvent.value,
+                        3000)
+                    pygame.time.set_timer(
+                        GameConstants.GameEvents.SpeedInvader2SpawnEvent.value,
+                        4500)
+            case 8:
+                for i in range(5):
+                    pygame.time.set_timer(
+                        GameConstants.GameEvents.CommonInvader2SpawnEvent.value,
+                        1500, 2)
+                    pygame.time.set_timer(
+                        GameConstants.GameEvents.SpeedInvader2SpawnEvent.value,
+                        3000, 2)
+                pygame.time.set_timer(
+                    GameConstants.GameEvents.TankInvader1SpawnEvent.value,
+                    4500)
+            case 9:
+                for i in range(3):
+                    pygame.time.set_timer(
+                        GameConstants.GameEvents.CommonInvader2SpawnEvent.value,
+                        1500, 4)
+                    pygame.time.set_timer(
+                        GameConstants.GameEvents.SpeedInvader2SpawnEvent.value,
+                        3000, 2)
+                    pygame.time.set_timer(
+                        GameConstants.GameEvents.TankInvader1SpawnEvent.value,
+                        4500)
+            case 10:
+                pass
+
+    @staticmethod
+    def spawnEnemy(type: GameConstants.EnemyType):
+
+        # common
+        if type == GameConstants.EnemyType.COMMONINVADER1:
+            EnemiesManager.list_enemies.append(CommonInvader1())
+
+        elif type == GameConstants.EnemyType.COMMONINVADER2:
+            EnemiesManager.list_enemies.append(CommonInvader2())
+
+        # speed
+        elif type == GameConstants.EnemyType.SPEEDINVADER1:
+            EnemiesManager.list_enemies.append(CommonInvader1())
+
+        elif type == GameConstants.EnemyType.SPEEDINVADER2:
+            EnemiesManager.list_enemies.append(SpeedInvader2())
+
+        # tank
+        elif type == GameConstants.EnemyType.TANKINVADER1:
+            EnemiesManager.list_enemies.append(TankInvader1())
+
+        # Boss
+        elif type == GameConstants.EnemyType.BOSS1:
+            EnemiesManager.list_enemies.append(Boss1())
--- python/main_game.py
+++ python/main_game.py
@@ -1,32 +1,103 @@
-import pygame
 import sys
-import classes
+
+from classes import *
+
+aspect_ratio = (3, 4)
 
 pygame.init()
 
-screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+'''
+screen_height = pygame.display.Info().current_h
+screen_width = int(screen_height / aspect_ratio[0] * aspect_ratio[1])
+screen = pygame.display.set_mode((screen_width, screen_height), pygame.RESIZABLE)
+
+height, width = 400, 300
+screen = pygame.display.set_mode((height, width), pygame.RESIZABLE)
+bg = pygame.image.load('../img/pixel art bg.png').convert_alpha()
+scale_bg = pygame.transform.scale(bg, (height, width))'''
+
+# Constants
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+ASPECT_RATIO = 4 / 3
+GAME_WIDTH = SCREEN_WIDTH
+GAME_HEIGHT = int(GAME_WIDTH / ASPECT_RATIO)
+BLACK = (0, 0, 0)
+GameConstants.win_size = [SCREEN_WIDTH, SCREEN_HEIGHT]
+
+# Create the Pygame window
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.RESIZABLE)
+
+
+fullscreen = False
+
 pygame.display.set_caption("Space Invaders")
-bg = pygame.image.load('../img/background.png')
 
 running = True
 
+EnemiesManager.list_enemies.append(CommonInvader1())
+EnemiesManager.list_enemies.append(SpeedInvader1())
 
-listeEnnemis = []
-for indice in range(classes.Ennemi.NbEnnemis):
-    vaisseau = classes.Ennemi()
-    listeEnnemis.append(vaisseau)
-0
+player = Player([random.randint(0, GameConstants.win_size[0]), GameConstants.win_size[1] - 100], "vaisseau")
 
+EnemiesManager.send_waves_levels(9)
+
+# screen.blit(bg, (0, 0))
 while running:
-    screen.blit(bg,(0,0))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
             sys.exit()
 
+        if event.type == GameConstants.GameEvents.CommonInvader1SpawnEvent.value:
+            EnemiesManager.spawnEnemy(GameConstants.EnemyType.COMMONINVADER1)
+        if event.type == GameConstants.GameEvents.CommonInvader2SpawnEvent.value:
+            EnemiesManager.spawnEnemy(GameConstants.EnemyType.COMMONINVADER2)
+        if event.type == GameConstants.GameEvents.SpeedInvader1SpawnEvent.value:
+            EnemiesManager.spawnEnemy(GameConstants.EnemyType.SPEEDINVADER1)
+        if event.type == GameConstants.GameEvents.SpeedInvader2SpawnEvent.value:
+            EnemiesManager.spawnEnemy(GameConstants.EnemyType.SPEEDINVADER2)
+        if event.type == GameConstants.GameEvents.TankInvader1SpawnEvent.value:
+            EnemiesManager.spawnEnemy(GameConstants.EnemyType.TANKINVADER1.value)
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_F11:
+                fullscreen = not fullscreen
+                if fullscreen:
+                    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.FULLSCREEN)
+                else:
+                    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.RESIZABLE)
+
+            if event.key == pygame.K_SPACE:
+                player.tirer()
+
+
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_q or event.key == pygame.K_d:
+                player.setSpeed(GameConstants.PlayerSpeed.STOP)
+
+    window_width, window_height = pygame.display.get_surface().get_size()
+
+    # Calculate the position to center the game on the window
+    game_x = (window_width - GAME_WIDTH) // 2
+    game_y = (window_height - GAME_HEIGHT) // 2
+    screen.fill(BLACK)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_q]:
+        player.setSpeed(GameConstants.PlayerSpeed.LEFT)
+    if keys[pygame.K_d]:
+        player.setSpeed(GameConstants.PlayerSpeed.RIGHT)
+
+    player.bouger()
+
+    for enemy in EnemiesManager.list_enemies:
+        enemy.avancer()
+        screen.blit(enemy.image, (enemy.depart, enemy.hauteur))
+
+    screen.blit(player.image, player.pos)
+
     pygame.display.update()
-
-
 
 pygame.quit()
